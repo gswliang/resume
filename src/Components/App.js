b7e10811f7bea7503e94f3f84609c43cf79de20b
Grace <gswliang@gmail.com>
2021-05-01 19:24:10 +0800

    education rwd has been completed

--- src/Components/App.js
+++ src/Components/App.js
@@ -14,8 +14,8 @@ const App = () => {
       <Description />
       <Experience />
       <Education />
-      {/* <Skills />
-      <Projects /> */}
+      <Skills />
+      <Projects />
     </div>
   );
 };
